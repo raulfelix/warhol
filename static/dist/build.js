5b30b6de8930dc192c7f6e3ba6036b94e659098b
Raul Carrizo <raul.carrizo@fluidintel.com>
2014-04-18 12:00:25 +1000

    #13: search modal JS and main layout

--- static/dist/build.js
+++ static/dist/build.js
@@ -33,28 +33,34 @@ window.LWA = window.LWA || { Views: {}, Modules: {} };
 /*
  *
  */
-LWA.Modules.Modal = (function() {
+LWA.Modules.Modal = function(triggerSelector, modalSelector) {
 
-  var $modal;
+  var $body, $modal;
 
-  function onClick(ev) {
+  function onOpen(ev) {
     ev.preventDefault();
     $modal.toggleClass('modal-active');
+    $body.toggleClass('no-scroll');
   }
 
   function onClose() {
     $modal.removeClass('modal-active');
+    $body.removeClass('no-scroll');
   }
 
+  function init() {
+    $body = $('body');
+    $modal = $(modalSelector);
+    $(triggerSelector).on('click', onOpen);
+    $('.modal-close', $modal).click(onClose);
+  }
+
+  init();
+
   return {
-    init: function(triggerSelector, modalSelector) {
-      $modal = $(modalSelector);
-      $(triggerSelector).on('click', onClick);
-      $('.modal-close', $modal).click(onClose);
-    }
   };
 
-})();
+};
 /* global LWA */
 window.LWA = window.LWA || { Views: {}, Modules: {} };
 
--- static/scripts/modal.js
+++ static/scripts/modal.js
@@ -4,25 +4,31 @@ window.LWA = window.LWA || { Views: {}, Modules: {} };
 /*
  *
  */
-LWA.Modules.Modal = (function() {
+LWA.Modules.Modal = function(triggerSelector, modalSelector) {
 
-  var $modal;
+  var $body, $modal;
 
-  function onClick(ev) {
+  function onOpen(ev) {
     ev.preventDefault();
     $modal.toggleClass('modal-active');
+    $body.toggleClass('no-scroll');
   }
 
   function onClose() {
     $modal.removeClass('modal-active');
+    $body.removeClass('no-scroll');
   }
 
+  function init() {
+    $body = $('body');
+    $modal = $(modalSelector);
+    $(triggerSelector).on('click', onOpen);
+    $('.modal-close', $modal).click(onClose);
+  }
+
+  init();
+
   return {
-    init: function(triggerSelector, modalSelector) {
-      $modal = $(modalSelector);
-      $(triggerSelector).on('click', onClick);
-      $('.modal-close', $modal).click(onClose);
-    }
   };
 
-})();
+};
