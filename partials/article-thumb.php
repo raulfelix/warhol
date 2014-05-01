154df3ef0487037312a928e2f9213e3656320f58
Raul Carrizo <raul.carrizo@fluidintel.com>
2014-05-01 22:20:07 +1000

    #6: standard thumbnail partial and layout

---
+++ partials/article-thumb.php
@@ -0,0 +1,17 @@
+<?php
+  /*
+   * Standard thumbnail layout
+   */
+  $category = category($post->post_type);
+?>
+
+<div class="thumb">
+  <a class="h-5 thumb-link" href="<?php echo $category['permalink']; ?>"><?php echo $category['name']; ?></a>
+  <div class="thumb-feature">
+     <?php the_post_thumbnail( 'large' ); ?>  
+    <span class="thumb-time"><?php when(); ?></span>
+    <span class="thumb-views data-views"><i class="icon-views"></i><?php views(); ?></span>
+  </div>
+  <a href="<?php echo the_permalink(); ?>" class="h-2 thumb-title"><?php the_title(); ?></a>
+  <div class="h-5 thumb-caption"><?php the_subtitle(); ?></div>
+</div>
