5b5aaeac4b9d93cbdb863f1dfcbf04503aced3c0
Raul Carrizo <raul.carrizo@fluidintel.com>
2014-05-16 16:54:21 +1000

    #48: category header reading in values

--- partials/header-category.php
+++ partials/header-category.php
@@ -5,12 +5,15 @@
     $img_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'original');
   } 
 
+  $heximus = get_post_meta( get_the_id(), 'heximus_key', true );
+  $alpha = get_post_meta( get_the_id(), 'heximus_alpha_key', true );
 ?>
 
 <header class="header feature category">
   <div class="feature-bg" style="background-image: url(<?php echo $img_url[0] ?>);"></div>
-  <div class="blanket"></div>
-
+  <?php if ($is_feature === true): ?>
+    <div class="blanket-overlay" style="background-color:<?php echo ($heximus) ? $heximus : '#000000' ?>;opacity: <?php echo ($alpha) ? $alpha : '0.8' ?>;"></div>
+  <?php endif; ?>
   <?php get_template_part('partials/header', 'nav'); ?>
 
   <div class="f-grid f-row">
@@ -21,14 +24,13 @@
           <div class="category-h2"><?php the_subtitle(); ?></div>
         </div>
         <div class="f-1-2 bp1-1 category-branding">
-          <div class="category-h2">in partnership with</div>
           <?php 
             $gallery = new Attachments( 'my_attachments', get_the_id() );
-            if( $gallery->exist() ) {
+            if ( $gallery->exist() ):
               echo $gallery->image( 'original', 0 );
-            }
+              echo '<div class="category-h2">in partnership with</div>';
+            endif;
           ?>
-          <!-- <img src="../images/red_bull.png"> -->
         </div>
       </div>
     </div>
