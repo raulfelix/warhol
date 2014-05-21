f94a05b3fc6ab20c912a3ac6f5c5b1342389f109
Raul Carrizo <raul.carrizo@fluidintel.com>
2014-05-21 21:26:28 +1000

    declaring character encoding

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 
 <html <?php language_attributes(); ?>>
 <head>
-  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width">
   <title></title>
 
