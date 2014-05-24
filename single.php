ebc748bee05789b8fc1b4732d0c6a235ac4f3f33
Raul Carrizo <raul.carrizo@fluidintel.com>
2014-05-24 17:55:09 +1000

    Adding Facebook like script to single template

--- single.php
+++ single.php
@@ -6,13 +6,30 @@
   get_header();
 ?>
 
+  <div id="fb-root"></div>
+  <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : 634830329945158,
+        xfbml      : true,
+        version    : 'v2.0'
+      });
+    };
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  </script>
+
   <?php
     
     if (have_posts()): while (have_posts()): the_post();
       get_template_part( 'partials/article', 'content' );
       get_template_part( 'partials/module', 'share' );
     endwhile; endif;
-    
   ?>
   
 <?php get_footer(); ?>
