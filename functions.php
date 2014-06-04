276a50c19a0527ab165e945da35fa4fbb53c7dc3
Raul Carrizo <raul.carrizo@fluidintel.com>
2014-06-04 14:01:09 +1000

    #62: updating article max width to 10 columns, added function to strip img wrapping p tag

--- functions.php
+++ functions.php
@@ -38,6 +38,16 @@ function post_remove () {
 add_action('admin_menu', 'post_remove'); 
 
 
+// ------------------------------ 
+// admin: 
+// remove wrapping of img elements
+// ------------------------------ 
+function filter_ptags_on_images($content){
+   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+add_filter('the_content', 'filter_ptags_on_images');
+
+
 // --------------------------------- 
 // register custom taxonomies
 //
