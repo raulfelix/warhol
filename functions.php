9ff6e3e46273d487458348d67f2568e02936682d
Raul Carrizo <raul.carrizo@fluidintel.com>
2014-06-07 14:19:29 +1000

    #58: Allow author and photos by to be added to a post

--- functions.php
+++ functions.php
@@ -139,7 +139,7 @@ function create_post_type() {
     'has_archive' => true,
     'rewrite' => array('slug' => 'featured/%featured_tax%', 'with_front' => false),
     'menu_position' => 5,
-    'supports' => array('author', 'title', 'editor', 'thumbnail', 'revisions' ),
+    'supports' => array('title', 'editor', 'thumbnail', 'revisions' ),
     'taxonomies' => array( 'featured_tax', 'subtitle', 'carousel' )
   );
 
@@ -155,7 +155,7 @@ function create_post_type() {
     'has_archive' => true,
     'rewrite' => array('slug' => 'news/%news_tax%', 'with_front' => false),
     'menu_position' => 5,
-    'supports' => array('author', 'title', 'editor', 'thumbnail', 'revisions' ),
+    'supports' => array('title', 'editor', 'thumbnail', 'revisions' ),
     'taxonomies' => array( 'news_tax', 'subtitle', 'carousel' )
   );
 
@@ -233,6 +233,16 @@ function add_heximus_meta_box() {
     'heximus_meta_box_callback',
     'page'
   );
+
+  $screens = array( 'lwa_feature', 'lwa_news' );
+  foreach ( $screens as $screen ) {
+    add_meta_box(
+      'credits_id',
+      'Author and photography',
+      'credits_meta_box_callback',
+      $screen
+    );
+  }
 }
 add_action( 'add_meta_boxes', 'add_heximus_meta_box' );
 
@@ -296,6 +306,68 @@ function heximus_save_meta_box_data( $post_id ) {
   update_post_meta( $post_id, 'heximus_alpha_key', $alpha );
 }
 add_action( 'save_post', 'heximus_save_meta_box_data' );
+
+
+function credits_meta_box_callback( $post ) {
+
+  // Add an nonce field so we can check for it later.
+  wp_nonce_field( 'credits_meta_box', 'credits_meta_box_nonce' );
+
+  $value = get_post_meta( $post->ID, 'credits_author_key', true );
+  $alpha = get_post_meta( $post->ID, 'credits_photos_key', true );
+
+  echo '<div><label class="label-credits" for="credits_author_new_field">';
+  _e( 'Words by', 'textdomain' );
+  echo '</label> ';
+  echo '<input class="c-credits widefat" type="text" id="credits_author_new_field" name="credits_author_new_field" value="' . esc_attr( $value ) . '"/></div>';
+  echo '<div><label class="label-credits-photos" for="credits_photos_new_field">';
+  _e( 'Photos by', 'textdomain' );
+  echo '</label> ';
+  echo '<input class="c-credits widefat" type="text" id="credits_photos_new_field" name="credits_photos_new_field" value="' . esc_attr( $alpha ) . '"/></div>';
+}
+
+function credits_save_meta_box_data( $post_id ) {
+
+  // Check if our nonce is set.
+  if ( ! isset( $_POST['credits_meta_box_nonce'] ) ) {
+    return;
+  }
+
+  // Verify that the nonce is valid.
+  if ( ! wp_verify_nonce( $_POST['credits_meta_box_nonce'], 'credits_meta_box' ) ) {
+    return;
+  }
+
+  // If this is an autosave, our form has not been submitted, so we don't want to do anything.
+  if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+    return;
+  }
+
+  // Check the user's permissions
+  if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+    if ( ! current_user_can( 'edit_page', $post_id ) ) {
+      return;
+    }
+  } else {
+    if ( ! current_user_can( 'edit_post', $post_id ) ) {
+      return;
+    }
+  }
+
+  // Make sure that data is set
+  if ( !isset( $_POST['credits_author_new_field'] ) && !isset( $_POST['credits_photos_new_field'] ) ) {
+    return;
+  }
+
+  // Sanitize input
+  $author = sanitize_text_field( $_POST['credits_author_new_field'] );
+  $photos = sanitize_text_field( $_POST['credits_photos_new_field'] );
+
+  // Update the meta field in the database.
+  update_post_meta( $post_id, 'credits_author_key', $author );
+  update_post_meta( $post_id, 'credits_photos_key', $photos );
+}
+add_action( 'save_post', 'credits_save_meta_box_data' );
 
 
 // ---------------------------------- 
--- partials/article-content.php
+++ partials/article-content.php
@@ -15,10 +15,21 @@
       <a class="button button-share" href="javascript:void(0)"><i class="icon-share"></i>share this</a>
     </div>
     <div class="details-row details-row-secondary">
-      Words by <?php echo get_the_author_meta('display_name'); ?>
-      <span class="bullet"></span>
-      Photos by Mitchel Tomlinson
-      <span class="bullet"></span>
+      <?php 
+        $words_by = get_post_meta( get_the_id(), 'credits_author_key', true );
+        $photos_by = get_post_meta( get_the_id(), 'credits_photos_key', true );
+      ?>
+
+      <?php if ($words_by): ?>
+        Words by <?php echo $words_by; ?>
+        <span class="bullet"></span>
+      <?php endif; ?>
+      
+      <?php if ($photos_by): ?>
+        Photos by <?php echo $photos_by; ?>
+        <span class="bullet"></span>
+      <?php endif; ?>
+      
       <?php echo get_the_time( get_option('date_format')) ?>
     </div>
   </div>
