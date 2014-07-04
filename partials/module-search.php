1ed199fb5f1ea28698cb7fb6fc883cc5e3d6123e
Raul Carrizo <raul.carrizo@fluidintel.com>
2014-07-04 15:19:48 +1000

    #119: fixing underline of search input on mobile

--- partials/module-search.php
+++ partials/module-search.php
@@ -5,6 +5,7 @@
       <a href="<?php echo get_home_url(); ?>" class="logo"></a>
       <div class="search-row-input">
         <input class="input-search" type="text" placeholder="Search for something">
+        <div class="input-search-underline"></div>
         <button id="js-search" class="button button-search button-loader button-icon" type="text">
           <span class="button-text text"><i class="icon icon-search"></i>Search</span>
           <?php get_template_part('partials/module', 'util-loader'); ?>
