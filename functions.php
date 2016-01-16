475af9aaeaa7b4c8f10e0e6eae045f9b7656d6b6
Raul Felix Carrizo <raulfelixcarrizo@gmai.com>
2016-01-16 11:23:22 +1100

    Fix share modal styling

--- functions.php
+++ functions.php
@@ -68,7 +68,7 @@ add_action('admin_enqueue_scripts', 'enqueue_custom_admin_scripts');
 // ------------------------------ 
 function add_scripts() {
 
-  wp_register_style( 'style', get_template_directory_uri() . '/static/dist/css/316cbba3e8f3.style.css', null, null, 'all' );
+  wp_register_style( 'style', get_template_directory_uri() . '/static/dist/css/2482d0f5ea31.style.css', null, null, 'all' );
 
   wp_register_script( 'global', get_template_directory_uri() . '/static/dist/js/0eb2ef19e2c2.global.min.js', null, null, true );
   wp_register_script( 'home', get_template_directory_uri() . '/static/dist/js/cd3fe0150a87.home.min.js', null, null, true );
