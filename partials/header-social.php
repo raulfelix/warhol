9ebd363c715c948c5ac06a58483c6e07888c31dd
Raul Felix Carrizo <raulfelixcarrizo@gmai.com>
2016-01-24 10:02:47 +1100

    #7: Add social links to header

--- partials/header-social.php
+++ partials/header-social.php
@@ -1,6 +1,6 @@
 <header class="header-social">
-  <a href="/" target="_blank" class="header-social-link icon-facebook"></a>
+  <a href="https://www.facebook.com/Life-Without-Andy-185331311539927/?fref=ts" target="_blank" class="header-social-link icon-facebook"></a>
   <a href="https://www.instagram.com/lifewithoutandy/" target="_blank" class="header-social-link icon-instagram"></a>
-  <a href="/" target="_blank" class="header-social-link icon-vimeo-with-circle"></a>
-  <a href="/" target="_blank" class="header-social-link icon-twitter"></a>
+  <a href="https://vimeo.com/lifewithoutandy" target="_blank" class="header-social-link icon-vimeo-with-circle"></a>
+  <a href="https://twitter.com/LifeWithoutAndy?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" class="header-social-link icon-twitter"></a>
 </header>
