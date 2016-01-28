c9bee1d398700088ed5a831c1d42b11ea0d94953
Raul Felix Carrizo <raulfelixcarrizo@gmai.com>
2016-01-28 22:48:18 +1100

    #15: most popular articles sidebar and ads

--- functions.php
+++ functions.php
@@ -68,7 +68,7 @@ add_action('admin_enqueue_scripts', 'enqueue_custom_admin_scripts');
 // ------------------------------ 
 function add_scripts() {
 
-  wp_register_style( 'style', get_template_directory_uri() . '/static/dist/css/12dd074fadf5.style.css', null, null, 'all' );
+  wp_register_style( 'style', get_template_directory_uri() . '/static/dist/css/8580b0ad7a5f.style.css', null, null, 'all' );
 
   wp_register_script( 'global', get_template_directory_uri() . '/static/dist/js/a34ee02c9949.global.min.js', null, null, true );
   wp_register_script( 'home', get_template_directory_uri() . '/static/dist/js/cd3fe0150a87.home.min.js', null, null, true );
