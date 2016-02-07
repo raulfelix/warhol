760ea4ab6b5b6524abc7ae99ed759796beda432d
Raul Felix Carrizo <raulfelixcarrizo@gmai.com>
2016-02-07 13:14:28 +1100

    breadcrumbs

--- partials/article-thumb.php
+++ partials/article-thumb.php
@@ -10,11 +10,14 @@
   <?php 
     if ($category['parent']):
   ?>
-  <a class="h-5 thumb-link" href="<?php echo $category['parentPermalink']; ?>"><?php echo $category['parent']; ?></a><span class="colon">:</span>
-  <?php 
+    <a class="h-5 thumb-link" href="<?php echo $category['parentPermalink']; ?>"><?php echo $category['parent']; ?></a><span class="colon">:</span><a class="h-5 thumb-link" href="<?php echo $category['permalink']; ?>"><?php echo $category['name']; ?></a>
+  <?php
+    else:
+    ?>
+      <a class="h-5 thumb-link" href="<?php echo $category['permalink']; ?>"><?php echo $category['name']; ?></a>
+  <?php
     endif;
   ?>
-  <a class="h-5 thumb-link" href="<?php echo $category['permalink']; ?>"><?php echo $category['name']; ?></a>
   </div>
   <a href="<?php echo the_permalink(); ?>" class="thumb-feature">
      <?php get_thumbnail(false, $post->post_type === 'lwa_news' ? true : false); ?>  
